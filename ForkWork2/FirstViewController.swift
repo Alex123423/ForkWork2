f2d878b923317632c00f269e6a674d365498751e
Alexey Davidenko <alexeydavidenko@icloud.com>
2022-01-19 15:03:57 +0300

    add FirstViewController and changed backgroundColor

---
+++ ForkWork2/FirstViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+    }
+
+
+    func setupView() {
+        view.backgroundColor = .red
+    }
+}

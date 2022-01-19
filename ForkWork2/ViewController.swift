d85f3fc3e259b9b4a8251100027014be9fa6f9f7
Alexey Davidenko <alexeydavidenko@icloud.com>
2022-01-19 15:01:56 +0300

    Initial Commit

---
+++ ForkWork2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForkWork2
+//
+//  Created by Alexey Davidenko on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
